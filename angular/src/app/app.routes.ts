7db31caeaf4fb899ad68f0eb81939e052a6e73d2
Roberto Devito <rdevito@frsn.utn.edu.ar>
2025-09-10 23:10:00 -0300

    arg

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -4,10 +4,11 @@ export const appRoutes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadChildren: () => import('./home/home.routes').then(m => m.homeRoutes),
-  },
-  {
-    path: 'menu',
     loadComponent: () => import('./menu/menu.component').then(m => m.MenuComponent),
-  }
+  },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   loadChildren: () => import('./home/home.routes').then(m => m.homeRoutes),
+  // },
 ];
