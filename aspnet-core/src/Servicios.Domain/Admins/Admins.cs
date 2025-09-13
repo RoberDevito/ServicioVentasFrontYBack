e1bdeb4c39ac3e4644d91739cb7e01e64b7249c8
wyn-code <brunoo6.massocco@gmail.com>
2025-09-13 07:09:03 -0300

    Creacion de tablas de clientes y admins

---
+++ aspnet-core/src/Servicios.Domain/Admins/Admins.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Entities.Auditing;
+
+
+namespace Servicios.Domain.Hamburguesa
+{
+    public class Admins : Entity<Guid>
+    {
+        public string name { get; set; } = null!;
+        public string lastname { get; set; } = null!;
+        public string email { get; set; } = null!;
+        public string password { get; set; } = null!;
+
+        public DateTime created { get; set; } = DateTime.Now;
+
+    }
+}
+
+
+
